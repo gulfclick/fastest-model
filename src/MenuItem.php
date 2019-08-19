23460b8b40a106c08970ab62de7150f798deb3d1
unknown <development@gulfclick.net>
2019-08-19 14:08:35 +0430

    add MenueItem Model

---
+++ src/MenuItem.php
@@ -0,0 +1,11 @@
+<?php
+namespace FastestModels;
+class MenuItem extends BaseModel
+{
+    public $timestamps=false;
+    protected $guarded=['id'];
+    public function views()
+    {
+        return $this->hasMany(MenuItem::class,'parent_id');
+    }
+}
