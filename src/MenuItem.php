3e8fa4315b3956e578f26f04878acdf8d39e73a7
unknown <development@gulfclick.net>
2019-08-19 16:31:55 +0430

    refactor model MenuItem

--- src/MenuItem.php
+++ src/MenuItem.php
@@ -2,13 +2,12 @@
 namespace FastestModels;
 class MenuItem extends BaseModel
 {
-    public $table='menu_item';
+    public $table='menu_items';
     public $timestamps=false;
     protected $guarded=['id'];
     public function views()
     {
         return $this->hasMany(MenuItem::class,'parent_id');
     }
-
 
 }
