66e2d16370507c79467afe800269df9bec7b28f6
unknown <development@gulfclick.net>
2019-09-01 16:24:44 +0430

    change user detail

--- src/UserDetail.php
+++ src/UserDetail.php
@@ -4,7 +4,7 @@ namespace FastestModels;
 class UserDetail extends BaseModel
 {
     protected $fillable = [
-        'user_id', 'user_type' /* [ client , driver , restaurant ] */, 'api_token', 'device_token', 'device_type' /* [android , ios] */, 'profile_photo', 'is_active'
+        'user_id', 'user_type' /* [ client , driver , restaurant,admin ] */, 'api_token', 'device_token', 'device_type' /* [android , ios] */, 'profile_photo', 'is_active'
     ];
 
     public function user()
