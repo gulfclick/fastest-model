120f5f711426639786f9812a99ef57d16737ec58
development@gulfclick.net <navid_hero_1st@yahoo.com>
2019-10-12 11:19:46 +0330

    add features

--- src/AppVersion.php
+++ src/AppVersion.php
@@ -4,4 +4,9 @@ namespace FastestModels;
 class AppVersion extends BaseModel
 {
     protected $guarded = ['id'];
+	
+	public function features()
+	{
+		return $this->hasMany(AppFeature::class, 'app_version_id', 'id');
+	}
 }
